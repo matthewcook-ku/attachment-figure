c2345b620c6d528f83c0f9f877eb556efddfc6de
Matthew Cook <77811379+matthewcook-ku@users.noreply.github.com>
2022-06-08 13:15:02 -0500

    Button crash fix and button hookup.
    Fixed (somehow) the null reference exception when buttons are pressed.
    Wired up remaining animation buttons and slider.

--- AF_Room/Assets/UI/Scripts/AnimationPanelController.cs
+++ AF_Room/Assets/UI/Scripts/AnimationPanelController.cs
@@ -9,53 +9,75 @@ public class AnimationPanelController : MonoBehaviour
     public void smileExpButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.MakeFace(AgentSkin.FaceExpression.Smile);
     }
     public void neutralExpButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.MakeFace(AgentSkin.FaceExpression.Neutral);
     }
     public void concernExpButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.MakeFace(AgentSkin.FaceExpression.Concern);
     }
     public void frownExpButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.MakeFace(AgentSkin.FaceExpression.Frown);
     }
     public void disgustExpButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.MakeFace(AgentSkin.FaceExpression.Disgust);
     }
     public void angerExpButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.MakeFace(AgentSkin.FaceExpression.Anger);
     }
     public void laughExpButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.MakeFace(AgentSkin.FaceExpression.Laugh);
     }
     public void shakeHeadButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.PerformBodyAction(AgentSkin.BodyAction.HeadShake);
     }
     public void nodHeadButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.PerformBodyAction(AgentSkin.BodyAction.HeadNod);
     }
-    public void leanForwardButtonPressed()
+    public void tiltLeftButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.PerformBodyAction(AgentSkin.BodyAction.HeadTiltLeft);
     }
-    public void leanNeutralButtonPressed()
+    public void tiltNeutralButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.PerformBodyAction(AgentSkin.BodyAction.HeadTiltNeutral);
     }
-    public void leanBackButtonPressed()
+    public void tiltRightButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.PerformBodyAction(AgentSkin.BodyAction.HeadTiltRight);
     }
-    public void crossLegsButtonPressed()
+    public void sitShiftButtonPressed()
     {
         Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.PerformBodyAction(AgentSkin.BodyAction.SitShift);
+    }
+    public void glanceShiftButtonPressed()
+    {
+        Debug.Log("Button Pressed: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        AFManager.Instance.agent.activeSkin.PerformBodyAction(AgentSkin.BodyAction.GlanceShift);
+    }
+    public void LeanValueSliderChanged(float value)
+    {
+        AFManager.Instance.agent.activeSkin.LeanDegree = value;
     }
 }
